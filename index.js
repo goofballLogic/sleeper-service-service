a1a4239f5085785eb5c1555ea5f3acb40640dee7
Andrew Stewart Gibson <andrew@goofballLogic.com>
2017-11-17 12:35:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const https = require( "https" );
-https.createServer( ( req, res ) => { res.writeHead( 200 ); res.end( "hi" ); } ).listen( process.env.PORT );
+const http = require( "http" );
+http.createServer( ( req, res ) => { res.writeHead( 200 ); res.end( "hi" ); } ).listen( process.env.PORT );
